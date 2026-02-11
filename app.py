42fde4b615a9be27d413c4f84c0b274dda4cf678
Christopher L. Lübbers <wsad@posteo.net>
2026-02-11 12:44:50 +0100

    routes and data structure

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import storage
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    blog_posts = storage.get_blogposts()
+    return render_template("index.html", posts=blog_posts)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ storage.py
@@ -0,0 +1,50 @@
+import os
+import json
+
+initial_blogposts = [
+    {
+        "id": 1,
+        "author": "John Doe",
+        "title": "First Post",
+        "content": "This is my first post.",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Second Post",
+        "content": "This is another post.",
+    },
+    # More blog posts can go here...
+]
+
+
+def get_blogposts():
+    """
+    Returns a list of dictionaries that
+    contains the blogpost information in the database.
+
+    The function loads the information from the JSON
+    file and returns the data.
+
+    For example, the function may return:
+    [
+    {"id": 1, "author": "John Doe", "title": "First Post", "content": "This is my first post."},
+    {"id": 2, "author": "Jane Doe", "title": "Second Post", "content": "This is another post."}
+    # More blog posts can go here...
+    ]
+    """
+    if not os.path.exists("data/data.json"):
+        print("data.json file not found. Creating a new one.")
+        with open("data/data.json", "w") as file:
+            json.dump({}, file)
+    with open("data/data.json", "r") as file:
+        blogposts = json.load(file)
+    return blogposts
+
+
+def save_blogposts(blogposts):
+    """
+    Gets all your movies as an argument and saves them to the JSON file.
+    """
+    with open("data/data.json", "w") as file:
+        json.dump(blogposts, file, indent=4)
